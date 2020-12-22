40cd27abf7578e2b5343fe5f86c604fc0545bd54
Corentin Martinez <co-martinez@orange.fr>
2020-12-22 11:29:10 +0100

    remove console log

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function changeimage() {
-    console.log("change image")
     var image = document.getElementById('imagerenard');
     if (image.src.includes("renard")== true) {
         image.src ="Ressources/image/hibou.jpg";
