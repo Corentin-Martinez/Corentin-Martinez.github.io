5f0ebb52a287d4639cefd23c2cf15458217ba53c
Corentin Martinez <co-martinez@orange.fr>
2021-01-05 14:11:03 +0100

    typing text mais bug au chgmt de page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-function changeimage() {
-    var image = document.getElementById('imagerenard');
+function changeimage(refimage) {
+    var image = document.getElementById(refimage);
     if (image.src.includes("renard")== true) {
         image.src ="Ressources/image/hibou.jpg";
     } else {
         image.src="Ressources/image/renard.jpg";
     }
 }
+
+
---
+++ js/typing.js
@@ -0,0 +1,80 @@
+var i = 0;
+var liste_typing = ['Géomaticien','Analyse de données','Développeur Web']
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+/* pour un mot*/
+function typeWriter(phrasetapee) {
+    if (i < phrasetapee.length) {
+        document.getElementById("demo").innerHTML += phrasetapee.charAt(i);
+        i++;
+        console.log('writing',i);
+        setTimeout(function(){typeWriter(phrasetapee)}, speed);
+    }
+}
+
+/* fonction suppression du mot*/
+function typedeleter(phrase) {
+    if (i > 0) {
+        console.log('deleting',i);
+        document.getElementById("demo").innerHTML = phrase.substring(0, i - 1);
+        i--;
+        console.log('mot = ',phrase);
+        setTimeout(function(){typedeleter(phrase)}, speed);
+    }
+}
+
+/* pour la liste */
+var j = 0
+function typeWriterListe() {
+    if (isInView) {
+        if (j> 0 ){
+            j--;
+            console.log('deleting word',liste_typing[j])
+            typedeleter(liste_typing[j])
+            j++;
+            if (j == liste_typing.length){
+                j = 0;
+            }
+        }
+        if (j < liste_typing.length && i == 0) {
+            console.log('writing word',liste_typing[j])
+            typeWriter(liste_typing[j])
+            j++ ;
+            console.log('j =', j);
+        }
+        setTimeout(typeWriterListe, 2000);
+
+    }
+
+}
+
+/* typing effect au moment ou le doc est pret */
+$(document).ready(function(){
+    $("#demo").bind('inview',typeWriterListe)
+});
+
+
+
+
+
+
+/* var i = 0;
+var liste_typing = ['Bonjour','Test','coucou tout le monde']
+var speed = 100;
+var j = 0;
+function typeWriterListe() {
+    if (j in liste_typing) {
+        console.log(j)
+        while (i < liste_typing[j].length) {
+            console.log('i =', i);
+            document.getElementById("demo").innerHTML += liste_typing[j].charAt(i);
+            i++;
+            
+          }
+        j++;
+        
+    }
+
+    setTimeout(typeWriterListe, speed);
+} */
